d70d74feba572ea19ce0eaeab84c36e1a4253f35
Diogo Gomes <dgomes@av.it.pt>
2017-11-24 00:38:39 +0000

    refactor

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     #send command
     stb = pymediaroom.Remote()
-    print(stb.get_status())
+    print(stb.get_standby())
     #stb.send_cmd("Info")
