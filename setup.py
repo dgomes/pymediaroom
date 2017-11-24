0017322467aa7cfedd7199248efb1f308cab2816
Diogo Gomes <dgomes@av.it.pt>
2017-11-24 00:40:37 +0000

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pymediaroom',
-      version='0.3',
+      version='0.4',
       description='Remote control your Mediaroom Set-up-box',
       url='http://github.com/dgomes/pymediaroom',
       author='Diogo Gomes',
