76a5e5910b3de04b1ab0690c3c87d29edb1c20ce
Diogo Gomes <dgomes@av.it.pt>
2018-01-19 21:20:27 +0000

    loop standby

--- app.py
+++ app.py
@@ -5,5 +5,6 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     #send command
     stb = pymediaroom.Remote()
-    print(stb.get_standby())
+    while(not stb.get_standby()):
+        print("OK")
     #stb.send_cmd("Info")
