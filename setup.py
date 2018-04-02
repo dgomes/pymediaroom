566bbd991ca0fe6461ce0dc7a94dd0253a8cedd5
Diogo Gomes <dgomes@av.it.pt>
2018-04-02 21:35:46 +0100

    update

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 from setuptools import setup
 
+long_description=open("README.rst").read()
+
 setup(name='pymediaroom',
-      version='0.5',
+      version='0.6.1',
       description='Remote control your Mediaroom Set-up-box',
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
       url='http://github.com/dgomes/pymediaroom',
       author='Diogo Gomes',
       author_email='diogogomes@gmail.com',
       license='MIT',
       packages=['pymediaroom'],
       install_requires=[
-          'xmltodict',
-          'asyncio',
-          'async_timeout',
+        'asyncio',
+        'async-timeout',
+        'xmltodict',
       ],
-      download_url= 'https://github.com/dgomes/pymediaroom/tarball/0.5',
+      python_requires='>=3.5',
+      download_url= 'https://github.com/dgomes/pymediaroom/tarball/0.6',
       )
